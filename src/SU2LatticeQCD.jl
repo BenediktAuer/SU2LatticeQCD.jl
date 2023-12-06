8ac49cdb282d93a79b7fa2a15bfd22fb90ec614c
BenediktAuer <benedikt_auer01@web.de>
2023-12-06 15:02:34 +0100

    updated includes

--- src/SU2LatticeQCD.jl
+++ src/SU2LatticeQCD.jl
@@ -1,8 +1,9 @@
 module SU2LatticeQCD
 using EfficentSU2
-import Base: size,getindex
+import Base: size,getindex,setindex!
 # Write your package code here.
 include("GaugeField.jl")
 include("periodic.jl")
+include("Metropolis.jl")
 export GaugeField2D,apply_periodic_boundary_conditions,getindexPeriodic
 end
