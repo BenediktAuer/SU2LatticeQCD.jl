3aa997e70db4ba664813868975cc1eb898d4efb4
BenediktAuer <benedikt_auer01@web.de>
2023-12-08 16:57:21 +0100

    small bug fixes

--- src/Metropolis.jl
+++ src/Metropolis.jl
@@ -3,30 +3,35 @@ function judge(ΔS)
     return (ΔS ≤ 0) || (rand(Float64) < exp(-ΔS))
 end
 
-function metropolis!(lattice,β,rounds)
-
-    Δ=2*sqrt(a)
+function metropolis!(lattice,β,iterator,rounds,ϵ)
+    #not sure about this one
+    #TODO:change to right value
+    N=1
     #first and last iteration missing
-    for i in eachindex(lattice)
+    @inbounds for i in iterator
         #calculate the staple for the given lattice site
         A = staple(lattice,i)
         for _ in 1:rounds
             #choose new Matrix
-            U′ = newSU2(ϵ)
-            #get Old MAtrix
             U = lattice[i]
+            U′ = newSU2(U,ϵ)
+            #get Old MAtrix
             #calculate the diffrence in Action from U′,U the staple and 
             DeltaS = ΔS(U′,U,A,β,N)
             #accept or reject U′
             if judge(DeltaS) 
-                lattice[i] = newx
+                #newx has to bea a copy. newSU2() makes such a copy ithink
+                lattice[i] .= U′
             end
         end
+        renormalize!(lattice[i])
     end
 
     return
 end
 #TODO: Maybe as inplace variant 
 function ΔS(U′,U,A,β,N=2)
-    return -β/N*tr((U′-U)*A)
+    diff= (U′-U)
+    temp = diff*A
+    return -β/N*tr(temp)
 end
--- src/SU2LatticeQCD.jl
+++ src/SU2LatticeQCD.jl
@@ -1,5 +1,5 @@
 module SU2LatticeQCD
-using EfficentSU2
+using EfficentSU2,LinearAlgebra,Random
 import Base: size,getindex,setindex!
 include("GaugeField.jl") 
 struct SU2Simulation
@@ -10,19 +10,21 @@ struct SU2Simulation
     Ny::Int64
     Nz::Int64
     Nt::Int64
-    function SU2Simulation(β,Nx,Ny,Nz,Nt)
+    ϵ::Float64
+    function SU2Simulation(β,Nx,Ny,Nz,Nt,ϵ)
         lattice = GaugeField4D(Nx,Ny,Nz,Nt)
         iterator = CartesianIndices(lattice)
-        return new(β,lattice,iterator,Nx,Ny,Nz,Nt)
+        return new(β,lattice,iterator,Nx,Ny,Nz,Nt,ϵ)
     end
 end
 function simulate!(a::SU2Simulation,rounds)
-    @inline metropolis!(a.:lattice,a.:β,a.:iterator,rounds)
+    @inline metropolis!(a.:lattice,a.:β,a.:iterator,rounds,a.:ϵ)
     
 end
 # Write your package code here.
 include("periodic.jl")
 include("Metropolis.jl")
 include("stapels.jl")
+include("RandomSU2.jl")
 export GaugeField2D,getDirectionalIndex,SU2Simulation, simulate!
 end
