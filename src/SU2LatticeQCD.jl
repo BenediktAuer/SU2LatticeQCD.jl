0b757292570c47afa83df69a339bffda215c063e
BenediktAuer <benedikt_auer01@web.de>
2023-12-08 13:07:07 +0100

    added simulate!

--- src/SU2LatticeQCD.jl
+++ src/SU2LatticeQCD.jl
@@ -1,21 +1,28 @@
 module SU2LatticeQCD
 using EfficentSU2
 import Base: size,getindex,setindex!
-struct SU2Simulation{N,T<:GaugeField{SU2{ComplexF32},N}}
+include("GaugeField.jl") 
+struct SU2Simulation
     β::Float32
-    lattice::T
+    lattice::GaugeField4D
     iterator::CartesianIndices
     Nx::Int64
     Ny::Int64
     Nz::Int64
     Nt::Int64
+    function SU2Simulation(β,Nx,Ny,Nz,Nt)
+        lattice = GaugeField4D(Nx,Ny,Nz,Nt)
+        iterator = CartesianIndices(lattice)
+        return new(β,lattice,iterator,Nx,Ny,Nz,Nt)
+    end
+end
+function simulate!(a::SU2Simulation,rounds)
+    @inline metropolis!(a.:lattice,a.:β,a.:iterator,rounds)
     
 end
-
 # Write your package code here.
-include("GaugeField.jl")
 include("periodic.jl")
 include("Metropolis.jl")
 include("stapels.jl")
-export GaugeField2D,getDirectionalIndex
+export GaugeField2D,getDirectionalIndex,SU2Simulation, simulate!
 end
