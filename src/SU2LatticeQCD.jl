73d3fe44b3d7911a04972ffeb9ed5f1651897073
BenediktAuer <benedikt_auer01@web.de>
2023-12-08 12:35:46 +0100

    added struct SU2Simulation

--- src/SU2LatticeQCD.jl
+++ src/SU2LatticeQCD.jl
@@ -1,6 +1,17 @@
 module SU2LatticeQCD
 using EfficentSU2
 import Base: size,getindex,setindex!
+struct SU2Simulation{N,T<:GaugeField{SU2{ComplexF32},N}}
+    β::Float32
+    lattice::T
+    iterator::CartesianIndices
+    Nx::Int64
+    Ny::Int64
+    Nz::Int64
+    Nt::Int64
+    
+end
+
 # Write your package code here.
 include("GaugeField.jl")
 include("periodic.jl")
