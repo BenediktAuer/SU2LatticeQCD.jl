a1f6d23b07e3f3bef9a600cac1584c69c6787071
BenediktAuer <benedikt_auer01@web.de>
2023-12-12 18:59:11 +0100

    IO working

--- src/IO.jl
+++ src/IO.jl
@@ -3,6 +3,10 @@ function save(path, a)
         println(file,"Dims"," ", a.:Nx," ", a.:Ny," ", a.:Nz," ", a.:Nt)
         println(file,"β", " ", a.:β[])
         println(file,"ϵ", " ", a.:ϵ[])
+        println(file,"RNGState: ", " ", copy(Random.default_rng()))
+        for i in eachindex(a.:lattice)
+            println(file,convert(SU2{ComplexF64},a.:lattice[i]))
+        end
     end
 end
 
@@ -16,5 +20,19 @@ function loadConfig!(a,path)
         ϵ= parse(Float32,split(readline(file))[2])
         a.:ϵ[] = ϵ
         println("Set ϵ to $(ϵ)!")
+        rng = split(readline(file))
+        println(rng[2])
+        state= parse.(UInt64,split(join(rng[2:end]),('(',',',')'))[2:5])
+        println(state)
+        if occursin("Xoshiro",rng[2]) == true
+            copy!(Random.default_rng(),Xoshiro(state...))
+        else
+            throw(DomainError("The used Numbergenerator is not supported!"))
+        end
+        for i in eachindex(a.:lattice)
+            
+            a.:lattice[i].=parse.(ComplexF32, split(readline(file),('[',',', ']'))[2:3])
+        end
+        #
     end
 end
