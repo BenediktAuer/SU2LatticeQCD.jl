2aaa98b7dbd24823a974cb6d79b088e8e9312810
BenediktAuer <benedikt_auer01@web.de>
2023-12-14 17:15:47 +0100

    updated staple calculation

--- src/stapels.jl
+++ src/stapels.jl
@@ -1,9 +1,10 @@
 function staple(lattice,i)
     μ = i.I[end]
     iterator = Iterators.filter(x->x!=μ,1:4)
-    staple = sum( lattice[getDirectionalIndex(i,μ,ν)] *lattice[getDirectionalIndex(i,ν,μ)]'*lattice[getDirectionalIndex(i,0,ν)]' +lattice[getDirectionalIndex(i,μ-ν,ν)]'*lattice[getDirectionalIndex(i,-ν,μ)]'*lattice[getDirectionalIndex(i,-ν,ν)] for ν in iterator)
+    #staple = sum( lattice[getDirectionalIndex(i,μ,ν)] *lattice[getDirectionalIndex(i,ν,μ)]'*lattice[getDirectionalIndex(i,0,ν)]' +lattice[getDirectionalIndex(i,μ-ν,ν)]'*lattice[getDirectionalIndex(i,-ν,μ)]'*lattice[getDirectionalIndex(i,-ν,ν)] for ν in iterator)
     #[((SU2LatticeQCD.getDirectionalIndex(CartesianIndex(1,1,1,1,1),μ,ν)  , SU2LatticeQCD.getDirectionalIndex(CartesianIndex(1,1,1,1,1),ν,μ) , SU2LatticeQCD.getDirectionalIndex(CartesianIndex(1,1,1,1,1),0,ν))  ,( SU2LatticeQCD.getDirectionalIndex(CartesianIndex(1,1,1,1,1),μ-ν,ν) , SU2LatticeQCD.getDirectionalIndex(CartesianIndex(1,1,1,1,1),-ν,μ) , SU2LatticeQCD.getDirectionalIndex(CartesianIndex(1,1,1,1,1),-ν,ν)))  for ν in iterator]
     #TODO: getDirectionalIndex just for the addition to index i, new:
+    staple = sum( lattice[i+getDirectionalUnitvector(μ)+getNu(i,ν)]*(lattice[i+getDirectionalUnitvector(ν)+getNu(i,μ)])'*(lattice[i+getDirectionalUnitvector(0)+getNu(i,ν)]')+(lattice[i+getDirectionalUnitvector(μ)+getDirectionalUnitvector(ν,-1)+getNu(i,ν)])'*(lattice[i+getDirectionalUnitvector(ν,-1)+getNu(i,μ)]')*lattice[i+getDirectionalUnitvector(ν,-1)+getNu(i,ν)] for ν in iterator)
     # staple = [((CartesianIndex(1,1,1,1,1)+SU2LatticeQCD.getDirectionalUnitvector(μ)+SU2LatticeQCD.getNu(CartesianIndex(1,1,1,1,1),ν)  , CartesianIndex(1,1,1,1,1)+SU2LatticeQCD.getDirectionalUnitvector(ν)+SU2LatticeQCD.getNu(CartesianIndex(1,1,1,1,1),μ) , CartesianIndex(1,1,1,1,1)+SU2LatticeQCD.getDirectionalUnitvector(0)+SU2LatticeQCD.getNu(CartesianIndex(1,1,1,1,1),ν)) ,( CartesianIndex(1,1,1,1,1)+SU2LatticeQCD.getDirectionalUnitvector(μ)+SU2LatticeQCD.getDirectionalUnitvector(ν,-1)+SU2LatticeQCD.getNu(CartesianIndex(1,1,1,1,1),ν) , CartesianIndex(1,1,1,1,1)+SU2LatticeQCD.getDirectionalUnitvector(ν,-1)+SU2LatticeQCD.getNu(CartesianIndex(1,1,1,1,1),μ) , CartesianIndex(1,1,1,1,1)+SU2LatticeQCD.getDirectionalUnitvector(ν,-1)+SU2LatticeQCD.getNu(CartesianIndex(1,1,1,1,1),ν,)))  for ν in iterator]
 end
 
@@ -17,10 +18,10 @@ end
 #     signofμ = sign(μ)
 #     return i+mul*signofμ*(μ==1||μ==-1)*CartesianIndex(1,0,0,0,0)+mul*signofμ*(μ==2||μ==-2)*CartesianIndex(0,1,0,0,0)+mul*signofμ*(μ==3||μ==-3)*CartesianIndex(0,0,1,0,0)+mul*signofμ*(μ==4||μ==-4)*CartesianIndex(0,0,0,1,0)-CartesianIndex(0,0,0,0,i.I[end]-ν)
 # end
-function getNu(i::CartesianIndex,ν)
+@inline function getNu(i::CartesianIndex,ν)
     return -CartesianIndex(0,0,0,0,i.I[end]-ν)
 end
-function getDirectionalUnitvector(μ,mul=1)
+@inline function getDirectionalUnitvector(μ,mul=1)
     mul*((μ==1)*CartesianIndex(1,0,0,0,0)+(μ==2)*CartesianIndex(0,1,0,0,0)+(μ==3)*CartesianIndex(0,0,1,0,0)+(μ==4)*CartesianIndex(0,0,0,1,0))
     
 end
