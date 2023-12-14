1f32fb8f0cfcc92d7ff65f3e67aebe9a8b14fe5a
BenediktAuer <benedikt_auer01@web.de>
2023-12-14 16:39:48 +0100

    updated todos

--- src/stapels.jl
+++ src/stapels.jl
@@ -3,7 +3,8 @@ function staple(lattice,i)
     iterator = Iterators.filter(x->x!=μ,1:4)
     staple = sum( lattice[getDirectionalIndex(i,μ,ν)] *lattice[getDirectionalIndex(i,ν,μ)]'*lattice[getDirectionalIndex(i,0,ν)]' +lattice[getDirectionalIndex(i,μ-ν,ν)]'*lattice[getDirectionalIndex(i,-ν,μ)]'*lattice[getDirectionalIndex(i,-ν,ν)] for ν in iterator)
     #[((SU2LatticeQCD.getDirectionalIndex(CartesianIndex(1,1,1,1,1),μ,ν)  , SU2LatticeQCD.getDirectionalIndex(CartesianIndex(1,1,1,1,1),ν,μ) , SU2LatticeQCD.getDirectionalIndex(CartesianIndex(1,1,1,1,1),0,ν))  ,( SU2LatticeQCD.getDirectionalIndex(CartesianIndex(1,1,1,1,1),μ-ν,ν) , SU2LatticeQCD.getDirectionalIndex(CartesianIndex(1,1,1,1,1),-ν,μ) , SU2LatticeQCD.getDirectionalIndex(CartesianIndex(1,1,1,1,1),-ν,ν)))  for ν in iterator]
-    #TODO: getDirectionalIndex just for the addition to index i
+    #TODO: getDirectionalIndex just for the addition to index i, new:
+    # staple = [((CartesianIndex(1,1,1,1,1)+SU2LatticeQCD.getDirectionalUnitvector(μ)+SU2LatticeQCD.getNu(CartesianIndex(1,1,1,1,1),ν)  , CartesianIndex(1,1,1,1,1)+SU2LatticeQCD.getDirectionalUnitvector(ν)+SU2LatticeQCD.getNu(CartesianIndex(1,1,1,1,1),μ) , CartesianIndex(1,1,1,1,1)+SU2LatticeQCD.getDirectionalUnitvector(0)+SU2LatticeQCD.getNu(CartesianIndex(1,1,1,1,1),ν)) ,( CartesianIndex(1,1,1,1,1)+SU2LatticeQCD.getDirectionalUnitvector(μ)+SU2LatticeQCD.getDirectionalUnitvector(ν,-1)+SU2LatticeQCD.getNu(CartesianIndex(1,1,1,1,1),ν) , CartesianIndex(1,1,1,1,1)+SU2LatticeQCD.getDirectionalUnitvector(ν,-1)+SU2LatticeQCD.getNu(CartesianIndex(1,1,1,1,1),μ) , CartesianIndex(1,1,1,1,1)+SU2LatticeQCD.getDirectionalUnitvector(ν,-1)+SU2LatticeQCD.getNu(CartesianIndex(1,1,1,1,1),ν,)))  for ν in iterator]
 end
 
 """
@@ -11,8 +12,17 @@ end
     returns the Index in diection μ at ν,i.e ``U \\_nu (x+\\mu)``
 
 """
-function getDirectionalIndex(i::CartesianIndex,μ,ν,mul=1)
-    #TODO anstatt sign use multiplicity
-    signofμ = sign(μ)
-    return i+mul*signofμ*(μ==1||μ==-1)*CartesianIndex(1,0,0,0,0)+mul*signofμ*(μ==2||μ==-2)*CartesianIndex(0,1,0,0,0)+mul*signofμ*(μ==3||μ==-3)*CartesianIndex(0,0,1,0,0)+mul*signofμ*(μ==4||μ==-4)*CartesianIndex(0,0,0,1,0)-CartesianIndex(0,0,0,0,i.I[end]-ν)
+# function getDirectionalIndex(i::CartesianIndex,μ,ν,mul=1)
+#     #TODO anstatt sign use multiplicity
+#     signofμ = sign(μ)
+#     return i+mul*signofμ*(μ==1||μ==-1)*CartesianIndex(1,0,0,0,0)+mul*signofμ*(μ==2||μ==-2)*CartesianIndex(0,1,0,0,0)+mul*signofμ*(μ==3||μ==-3)*CartesianIndex(0,0,1,0,0)+mul*signofμ*(μ==4||μ==-4)*CartesianIndex(0,0,0,1,0)-CartesianIndex(0,0,0,0,i.I[end]-ν)
+# end
+function getNu(i::CartesianIndex,ν)
+    return -CartesianIndex(0,0,0,0,i.I[end]-ν)
 end
+function getDirectionalUnitvector(μ,mul=1)
+    mul*((μ==1)*CartesianIndex(1,0,0,0,0)+(μ==2)*CartesianIndex(0,1,0,0,0)+(μ==3)*CartesianIndex(0,0,1,0,0)+(μ==4)*CartesianIndex(0,0,0,1,0))
+    
+end
+#Proposal shold be right
+# [((CartesianIndex(1,1,1,1,1)+SU2LatticeQCD.getDirectionalUnitvector(μ)+SU2LatticeQCD.getNu(CartesianIndex(1,1,1,1,1),ν)  , CartesianIndex(1,1,1,1,1)+SU2LatticeQCD.getDirectionalUnitvector(ν)+SU2LatticeQCD.getNu(CartesianIndex(1,1,1,1,1),μ) , CartesianIndex(1,1,1,1,1)+SU2LatticeQCD.getDirectionalUnitvector(0)+SU2LatticeQCD.getNu(CartesianIndex(1,1,1,1,1),ν)) ,( CartesianIndex(1,1,1,1,1)+SU2LatticeQCD.getDirectionalUnitvector(μ)+SU2LatticeQCD.getDirectionalUnitvector(ν,-1)+SU2LatticeQCD.getNu(CartesianIndex(1,1,1,1,1),ν) , CartesianIndex(1,1,1,1,1)+SU2LatticeQCD.getDirectionalUnitvector(ν,-1)+SU2LatticeQCD.getNu(CartesianIndex(1,1,1,1,1),μ) , CartesianIndex(1,1,1,1,1)+SU2LatticeQCD.getDirectionalUnitvector(ν,-1)+SU2LatticeQCD.getNu(CartesianIndex(1,1,1,1,1),ν,)))  for ν in iterator]
