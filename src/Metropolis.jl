8b5b0d66fda5277513537a3852795991c6f0aa1a
BenediktAuer <benedikt_auer01@web.de>
2023-12-26 14:18:20 +0100

    added parallel version

--- src/Metropolis.jl
+++ src/Metropolis.jl
@@ -3,7 +3,7 @@ function judge(ΔS)
     return (ΔS ≤ 0) || (rand(Float64) < exp(-ΔS))
 end
 
-function metropolis!(lattice,β,iterator,rounds,ϵ)
+function metropolis!(lattice::T,β,iterator,rounds,ϵ) where T<: GaugeField
     #not sure about this one
     #TODO:change to right value
     N=2
@@ -25,6 +25,36 @@ function metropolis!(lattice,β,iterator,rounds,ϵ)
             end
         end
         renormalize!(lattice[i])
+    end
+
+    return
+end
+#paralell implementation
+function metropolis!(lattice::T,β,iterator,rounds,ϵ) where T<: GaugeField4DP
+    #not sure about this one
+    #TODO:change to right value
+    N=2
+    #
+    for parity in [-1,1]
+        for μ in [1,2,3,4]
+            @tturbo for i in Iterators.filter(x->(-1)^sum(x.I)==parity,indices)
+        #calculate the staple for the given lattice site
+            A = staple(lattice,CartesianIndex(i,μ))
+            for _ in 1:rounds
+            #choose new Matrix
+                U = lattice[i,μ]
+                U′ = newSU2(U,ϵ)
+                #get Old MAtrix
+                #calculate the diffrence in Action from U′,U the staple and 
+                DeltaS = ΔS(U′,U,A,β,N)
+            #accept or reject U′
+                if judge(DeltaS) 
+                #newx has to bea a copy. newSU2() makes such a copy ithink
+                    lattice[i,μ] .= U′
+                end
+            end
+            renormalize!(lattice[i,μ])
+        end
     end
 
     return
