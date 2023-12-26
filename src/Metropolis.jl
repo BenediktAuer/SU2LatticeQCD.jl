82fb99db16faa26c715481b84a92992d1948ecdc
BenediktAuer <benedikt_auer01@web.de>
2023-12-26 16:07:20 +0100

    added multithreaded implementation

--- src/Metropolis.jl
+++ src/Metropolis.jl
@@ -37,28 +37,28 @@ function metropolis!(lattice::T,β,iterator,rounds,ϵ) where T<: GaugeField4DP
     #
     for parity in [-1,1]
         for μ in [1,2,3,4]
-            @tturbo for i in Iterators.filter(x->(-1)^sum(x.I)==parity,indices)
+            Threads.@threads  for i in collect(Iterators.filter(x->(-1)^sum(x.I)==parity,iterator))
         #calculate the staple for the given lattice site
-            A = staple(lattice,CartesianIndex(i,μ))
-            for _ in 1:rounds
-            #choose new Matrix
-                U = lattice[i,μ]
-                U′ = newSU2(U,ϵ)
-                #get Old MAtrix
-                #calculate the diffrence in Action from U′,U the staple and 
-                DeltaS = ΔS(U′,U,A,β,N)
-            #accept or reject U′
-                if judge(DeltaS) 
-                #newx has to bea a copy. newSU2() makes such a copy ithink
-                    lattice[i,μ] .= U′
-                end
+                    A = staple(lattice,CartesianIndex(i,μ))
+                    for _ in 1:rounds
+                    #choose new Matrix
+                        U = lattice[i,μ]
+                        U′ = newSU2(U,ϵ)
+                        #get Old MAtrix
+                        #calculate the diffrence in Action from U′,U the staple and 
+                        DeltaS = ΔS(U′,U,A,β,N)
+                    #accept or reject U′
+                        #removing the dot in .= changes algo ??
+                            lattice[i,μ] = ifelse(judge(DeltaS) , U′,U)
+                    end
+                renormalize!(lattice[i,μ])
             end
-            renormalize!(lattice[i,μ])
         end
     end
 
     return
 end
+
 #TODO: Maybe as inplace variant 
 function ΔS(U′,U,A,β,N=2)
     diff= (U′-U)
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,25 @@
 using SU2LatticeQCD
-using Test
-
-@testset "SU2LatticeQCD.jl" begin
+using Test, Statistics, BenchmarkTools
+function testfunction(i,execution)
+    a = SU2Simulation(2.0,4,4,4,2,0.2,execution)
+    res = zeros(Float64,i)
+    for i in 1:i
+    simulate!(a,100)
+    res[i] = measurmentloopSpacial(a,Polyakovloop,2)
+    end
+    mean(abs,res)
+    end
+    @testset "parallel" begin
+        # Write your tests here.
+        res = testfunction(10_000,:parallel)
+        @show res
+        @test  res≈0.44 atol=0.01
+        # @btime testfunction(10_000)
+    end
+@testset "serial execution" begin
     # Write your tests here.
-    @test true
+    res = testfunction(10_000,:serial)
+    @show res
+    @test  res ≈0.44 atol=0.02
+    # @btime testfunction(10_000)
 end
