0845261df3b5b1f9817e42e24fcefce7313edcaf
BenediktAuer <benedikt_auer01@web.de>
2024-01-08 15:54:19 +0100

    added Logging

--- src/IO.jl
+++ src/IO.jl
@@ -16,14 +16,13 @@ function loadConfig!(a,path)
         (dims[1]==a.:Nx)||(dims[2]==a.:Ny)||(dims[3]==a.:Nz)||(dims[4]==a.:Nt)||throw(DimensionMismatch("The Dimension of the given Simulation dont match the Dimension from the saved Simulation"))
         β= parse(Float32,split(readline(file))[2])
         a.:β[]=β
-        println("Set β to $(β)!")
+        @debug ("Set β to $(β)!")
         ϵ= parse(Float32,split(readline(file))[2])
         a.:ϵ[] = ϵ
-        println("Set ϵ to $(ϵ)!")
+        @debug ("Set ϵ to $(ϵ)!")
         rng = split(readline(file))
-        println(rng[2])
+        @debug (rng[2])
         state= parse.(UInt64,split(join(rng[2:end]),('(',',',')'))[2:5])
-        println(state)
         if occursin("Xoshiro",rng[2]) == true
             copy!(Random.default_rng(),Xoshiro(state...))
         else
--- src/SU2LatticeQCD.jl
+++ src/SU2LatticeQCD.jl
@@ -1,5 +1,5 @@
 module SU2LatticeQCD
-using EfficentSU2,LinearAlgebra,Random,Statistics,ThreadsX, StaticArrays
+using EfficentSU2,LinearAlgebra,Random,Statistics,ThreadsX, StaticArrays, Logging
 import Base: size,getindex,setindex!,show
 include("GaugeField.jl") 
 include("Acceptens.jl")
